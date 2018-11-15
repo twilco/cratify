29dbe57bc9f216228e5612b06c80ad0228e58d88
Tyler Wilcock <tyler.l.wilcock@gmail.com>
2018-11-14 23:04:07 -0600

    Rough implementation of frontend portion of signup

--- frontend/src/api-sdk/sdk.ts
+++ frontend/src/api-sdk/sdk.ts
@@ -1,1 +1,13 @@
-// TODO: Add API SDK methods here
+import request from './request'
+
+export const signup = async (username: string, password: string) => {
+  try {
+    const resp = await request('/signup', { body: JSON.stringify({
+      password,
+      username,
+    }), method: 'POST' })
+    return resp.response.json()
+  } catch (e) {
+    console.error(`error attempting to sign up: ${e}`)
+  }
+}
